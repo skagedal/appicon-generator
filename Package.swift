669a7f0048f8460e417ad481172e9b85baf3726d
Simon Kågedal Reimer <skagedal@gmail.com>
2018-08-21 07:07:45 +0200

    Make it actually generate a library

--- Package.swift
+++ Package.swift
@@ -1,17 +1,25 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "appicon-generator",
+    products: [
+        .library(
+            name: "AppIconGeneratorCore",
+            type: .static,
+            targets: ["AppIconGeneratorCore"]
+        ),
+       .executable(
+           name: "appicon-generator",
+           targets: ["appicon-generator"]
+       )
+    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "appicon-generator",
-            dependencies: ["AppIconGeneratorCore"]),
-        .target(
-            name: "AppIconGeneratorCore")
+            name: "AppIconGeneratorCore"),
+       .target(
+           name: "appicon-generator",
+           dependencies: ["AppIconGeneratorCore"])
     ]
 )
